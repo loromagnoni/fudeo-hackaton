6c5adf0f4e7554e6037daf357c8e499970e88a40
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-04-05 23:37:02 +0200

    feat: create reactive job offer list

--- packages/job_offer_repository/lib/src/job_offer_repository.dart
+++ packages/job_offer_repository/lib/src/job_offer_repository.dart
@@ -1,10 +1,22 @@
+import 'package:rxdart/subjects.dart';
+
 class JobOffer {
   const JobOffer({required this.title});
   final String title;
 }
 
 class JobOfferRepository {
-  Future<List<JobOffer>> getJobOffers() {
-    return Future.value([const JobOffer(title: 'First')]);
+  final _jobOfferListController = BehaviorSubject<List<JobOffer>>();
+
+  Stream<List<JobOffer>> get jobOfferList async* {
+    yield* _jobOfferListController.stream;
+  }
+
+  Future<void> getJobOffers() async {
+    _jobOfferListController.add([const JobOffer(title: 'First')]);
+  }
+
+  void dispose() {
+    _jobOfferListController.close();
   }
 }
