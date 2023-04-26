d7ee2b1512f5a32aebf7bdde9565bc0a251cdb33
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-04-27 00:59:18 +0200

    feat: add favourites page

---
+++ lib/favourites/bloc/favourites_event.dart
@@ -0,0 +1,35 @@
+part of 'favourites_bloc.dart';
+
+abstract class FavouritesEvent extends Equatable {
+  const FavouritesEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class JobOfferListChange extends FavouritesEvent {
+  const JobOfferListChange(this.jobOfferList);
+
+  final List<JobOffer> jobOfferList;
+
+  @override
+  List<Object> get props => [jobOfferList];
+}
+
+class FreelanceListChange extends FavouritesEvent {
+  const FreelanceListChange(this.freelanceList);
+
+  final List<Freelance> freelanceList;
+
+  @override
+  List<Object> get props => [freelanceList];
+}
+
+class FavouriteListChange extends FavouritesEvent {
+  const FavouriteListChange(this.favouriteList);
+
+  final List<String> favouriteList;
+
+  @override
+  List<Object> get props => [favouriteList];
+}
